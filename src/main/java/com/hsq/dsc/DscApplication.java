cf3a6018c6c77cdbab91d6ccae542c3ad25816f9
helongjun <lanrenspace@163.com>
2023-03-15 21:54:02 +0800

    初始化提交

---
+++ src/main/java/com/hsq/dsc/DscApplication.java
@@ -0,0 +1,13 @@
+package com.hsq.dsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscApplication.class, args);
+	}
+
+}
