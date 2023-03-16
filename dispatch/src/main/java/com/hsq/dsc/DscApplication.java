9bc24a0d31ea1074fd5546d62c14f3f0b13509ce
helongjun <lanrenspace@163.com>
2023-03-16 15:54:01 +0800

    添加测试

--- dispatch/src/main/java/com/hsq/dsc/DscApplication.java
+++ dispatch/src/main/java/com/hsq/dsc/DscApplication.java
@@ -1,8 +1,10 @@
 package com.hsq.dsc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.hsq.dsc.mapper")
 @SpringBootApplication
 public class DscApplication {
 
---
+++ dispatch/src/main/java/com/hsq/dsc/controller/FollowTaskController.java
@@ -0,0 +1,14 @@
+package com.hsq.dsc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: FollowTaskController
+ * @Author: LanRenSpace
+ * @Description: TODO
+ **/
+@RequestMapping("/followTask")
+@RestController
+public class FollowTaskController {
+}
